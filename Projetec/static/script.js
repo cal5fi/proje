33fcf1d81267fb2e1ffb69f8e76fc8a4fd55ee02
Calefi <108816288+cal5fi@users.noreply.github.com>
2023-02-16 15:44:22 -0300

    Add files via upload

---
+++ Projetec/static/script.js
@@ -0,0 +1,26 @@
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click",moveMenuLateral);
+
+function moveMenuLateral(){
+
+    if(menuMobile.classList.contains("bi bi-list")){
+        menuMobile.classList.replace("bi-list", "bi-x");
+    }else{
+        menuMobile.classList.replace("bi-x", "bi-list");
+    }
+
+    body.classList.toggle("menu-nav-active");
+
+}
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) =>{
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");        }
+    });
+});
